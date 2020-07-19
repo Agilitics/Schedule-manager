ed4f03e16180373f5cf9af266d6ff4468d6584a6
Harshitha397 <harshiharshi003@gmail.com>
2020-07-18 20:33:38 -0500

    Feature: ASM-3 - Added a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/Menu';
+import { MatButtonModule } from '@angular/material/Button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
